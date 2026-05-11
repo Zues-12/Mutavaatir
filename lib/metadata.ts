7b6266296d1d49b138fe16470114e6d0097859d6
Zunair Saeed <zunair.saeed@arhamsoft.com>
2026-05-11 20:33:09 +0500

    updated the icon

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -46,20 +46,8 @@ export const rootMetadata: Metadata = {
     },
   },
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/Mutavaatir-circle.ico',
+    apple: '/Mutavaatir-circle.ico',
+    shortcut: '/Mutavaatir-circle.ico',
   },
 }
